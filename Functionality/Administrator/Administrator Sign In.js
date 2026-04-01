103b3965810915e9f564d7beeab46aa1b44b1f01
Kamogelo Lebelo <lebelolesley@gmail.com>
2026-04-01 03:34:00 +0200

    Administrator Form Validations

---
+++ Functionality/Administrator/Administrator Sign In.js
@@ -0,0 +1,16 @@
+document.getElementById("Admin-SignIn-Form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let employeeNumber = document.getElementById("empNum").value;
+    let employeePassword = document.getElementById("empPassword").value;
+    let employeeError = document.getElementById("Admin-Login-Error");
+
+    if (employeeNumber.trim() === "" || employeePassword.trim() === "") {
+        employeeError.textContent = "All fields are required!";
+    }else  if (isNaN(employeeNumber)) {
+        employeeError.textContent = "Employee Number must be numeric!"
+    } else {
+        employeeError.textContent = "";
+        alert("Login Successful!");
+    }
+});
